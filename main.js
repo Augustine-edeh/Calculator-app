0318eeb6cb11aa7d637d8cd530a6b7bb6f4e1803
Augustine Edeh <info.augustinesedeh@gmail.com>
2023-02-03 10:46:08 +0100

    Delete function: improved function efficiency

--- main.js
+++ main.js
@@ -32,12 +32,8 @@ equalsKey.addEventListener("click", () => evaluate());
 // || DELETE FUNCTION
 const deleteKey = document.querySelector(".delete-key");
 const del = () => {
-  let lastValue = operationDisplay.value[operationDisplay.value.length - 1];
   if (operationDisplay.value.length !== 0) {
-    operationDisplay.value = operationDisplay.value.slice(
-      0,
-      operationDisplay.value.length - 1
-    );
+    operationDisplay.value = operationDisplay.value.slice(0, -1);
   }
 };
 deleteKey.addEventListener("click", () => del());
