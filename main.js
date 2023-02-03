fe3637051575fe4eec31047d5cb7fc0c486a4f71
Augustine Edeh <info.augustinesedeh@gmail.com>
2023-02-03 09:43:46 +0100

    Implemented delete function

--- main.js
+++ main.js
@@ -28,3 +28,16 @@ const evaluate = () => {
   solutionDisplay.value = eval(operationDisplay.value);
 };
 equalsKey.addEventListener("click", () => evaluate());
+
+// || DELETE FUNCTION
+const deleteKey = document.querySelector(".delete-key");
+const del = () => {
+  let lastValue = operationDisplay.value[operationDisplay.value.length - 1];
+  if (operationDisplay.value.length !== 0) {
+    operationDisplay.value = operationDisplay.value.slice(
+      0,
+      operationDisplay.value.length - 1
+    );
+  }
+};
+deleteKey.addEventListener("click", () => del());
