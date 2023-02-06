2e0f6dc29faab1fcbd9f73b570f3a954eed6074e
Augustine Edeh <info.augustinesedeh@gmail.com>
2023-02-07 00:10:21 +0100

    Evaluation: Upgraded anonymous function to arrow function

--- main.js
+++ main.js
@@ -70,7 +70,7 @@ const evaluate = () => {
 
   let answer = operationDisplay.value.replace(
     /(\+)|(−)|(×)|(÷)|(%)|(²)/gi,
-    function replacer($$) {
+    ($$) => {
       if ($$ == "+") {
         return $$;
       }
